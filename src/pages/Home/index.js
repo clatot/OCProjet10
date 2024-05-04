8bda84090c03a508e78dfb9ffc19d752d0ca3809
clatot <kat.clatot@gmail.com>
2024-05-04 16:04:45 +0200

    Changement const last

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  const last = data ? data.events[data.events.length - 1] : null;
   return <>
     <header>
       <Menu />
@@ -115,14 +116,13 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+      <h3>Notre derniére prestation</h3>
+      <EventCard
+        imageSrc={last?.cover}
+        title={last?.title}
+        date={new Date(last?.date)}
+        small label={last?.type}
+      />
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
